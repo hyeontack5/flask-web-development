197ae7cd4afdda56bd279ac47da772603e0ca5b3
hyeontack5 <sksgusxor@gmail.com>
2021-12-05 21:20:33 +0900

    Feat: 리퀘스트 컨텍스트

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "<h1>Hello World!</h1>"
+    user_agent = request.headers.get("User-Agent")
+    return "<p>Your browser is %s</p>" % user_agent
+
+
+# @app.route("/")
+# def index():
+#     return "<h1>Hello World!</h1>"
 
 
 @app.route("/user/<name>")
