97289ab2b7f2bf95cf211b9979347320eaea6369
hyeontack5 <sksgusxor@gmail.com>
2021-12-05 21:33:08 +0900

    Feat: make_response() 함수

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from flask import Flask
 from flask import request
+from flask import make_response
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    user_agent = request.headers.get("User-Agent")
-    return "<p>Your browser is %s</p>" % user_agent
+    response = make_response("<h1>This document carries a cookie!</h1>")
+    response.set_cookie("answer", "42")
+    return response
+
+
+# @app.route("/")
+# def index():
+#     user_agent = request.headers.get("User-Agent")
+#     return "<p>Your browser is %s</p>" % user_agent
 
 
 # @app.route("/")
