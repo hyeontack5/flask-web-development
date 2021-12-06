101b89b18db0e5db4722ecb91b2109535d365ee2
hyeontack5 <sksgusxor@gmail.com>
2021-12-06 13:33:56 +0900

    Feat: flask-script를 이용한 커맨드 라인 옵션

--- main.py
+++ main.py
@@ -1,38 +1,20 @@
 from flask import Flask
-from flask import request
-from flask import make_response
-from flask import redirect
+from flask_script import Manager
 
 app = Flask(__name__)
 
+manager = Manager(app)
 
-@app.route("/")
+
+@app.route('/')
 def index():
-    return redirect("http://localhost:5000/user/redirect-test")
+    return '<h1>Hello World!</h1>'
 
 
-# @app.route("/")
-# def index():
-#     response = make_response("<h1>This document carries a cookie!</h1>")
-#     response.set_cookie("answer", "42")
-#     return response
-
-
-# @app.route("/")
-# def index():
-#     user_agent = request.headers.get("User-Agent")
-#     return "<p>Your browser is %s</p>" % user_agent
-
-
-# @app.route("/")
-# def index():
-#     return "<h1>Hello World!</h1>"
-
-
-@app.route("/user/<name>")
+@app.route('/user/<name>')
 def user(name):
-    return "<h1>Hello, %s!</h1>" % name
+    return '<h1>Hello, %s!</h1>' % name
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    manager.run()
